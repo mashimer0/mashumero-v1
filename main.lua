ea2b3cca65dc612252177480b2f584f8efa5b0cd
mashimer0 <syoueiwatanabe010@gmail.com>
2026-09-18 15:09:06 +0900

    main.lua を更新

--- main.lua
+++ main.lua
@@ -2998,7 +2998,7 @@ end
                     blobRoot.CFrame = lookAtCFrame
                     blobRoot.Velocity = Vector3.zero
                     
-                    if tick() - packetTimer > 0.01 then  -- ★★★ 爆速！0.000000005 ★★★
+                    if tick() - packetTimer > 0.01 then  -- ★★★ 爆速！ 0.00005 ★★★
                         packetTimer = tick()
                         
                         -- ★★★ 両手交互掴み ★★★
